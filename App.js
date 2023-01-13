25f61b8c4e72b88a48d736e010d4ab71f451fee6
Max Ast <maxast31@gmail.com>
2023-01-13 12:22:59 +0100

    add repro

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+import { SafeAreaView, View, Pressable } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function App() {
+  const [date, setDate] = useState(new Date());
+  const [toggle, setToggle] = useState(false);
+
+  const height = useSharedValue(150);
+
+  const style = useAnimatedStyle(() => {
+    return {
+      height: withTiming(height.value),
+    };
+  });
+
+  const onDateChange = (_, selectedDate) => {
+    setDate(selectedDate);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}
+    >
+      <View style={{ flex: 1, paddingHorizontal: 20, marginTop: 60 }}>
+        <Pressable
+          onPress={() => {
+            setToggle((toggle) => !toggle);
+            if (height.value === 150) {
+              height.value = 300;
+            } else {
+              height.value = 150;
+            }
+          }}
+        >
+          <Animated.View style={[{ backgroundColor: "orange" }, style]}>
+            {toggle && (
+              <DateTimePicker
+                value={date}
+                mode="date"
+                is24Hour={true}
+                onChange={onDateChange}
+              />
+            )}
+          </Animated.View>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
